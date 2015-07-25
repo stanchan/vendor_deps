383e45fc6ed13f2850e0d0c11a5c102a6c6bf58e
Chan, Stanley <stchan@visa.com>
2015-07-24 21:10:45 -0700

    Add filesystem cookbook

---
+++ filesystem/metadata.rb
@@ -0,0 +1,19 @@
+name             'filesystem'
+maintainer       'Alex Trull'
+maintainer_email 'cookbooks.atrullmdsol@trull.org'
+description      'Installs/Configures various filesystems'
+license          'Apache 2.0'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.10.1"
+
+%w/redhat centos xenserver ubuntu debian scientific amazon/.each do |os|
+  supports os
+end
+
+depends 'lvm', '~> 1.1'
+
+attribute 'filesystems',
+  :description => "Filesystems to be created and/or mounted",
+  :type => "hash",
+  :required => "recommended"
+
