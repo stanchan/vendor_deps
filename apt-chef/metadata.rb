bca082471268fc9c48fe528bdadfadce877bf8ee
Stan Chan <stanchan@gmail.com>
2016-02-08 15:26:44 -0800

    Fix wrong metadata.rb for apt-chef cookbook

--- apt-chef/metadata.rb
+++ apt-chef/metadata.rb
@@ -1,15 +1,12 @@
-name 'yum-chef'
+name 'apt-chef'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license 'Apache 2.0'
-description 'Configures yum repository for Chef Software, Inc. products'
+description 'Configures apt repository for Chef Software, Inc. products'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.2.2'
-depends 'yum', '>= 3.2'
+depends 'apt'
+supports 'ubuntu'
 
-%w(amazon centos fedora oracle redhat scientific).each do |os|
-  supports os
-end
-
-source_url 'https://github.com/chef-cookbooks/yum-chef' if respond_to?(:source_url)
-issues_url 'https://github.com/chef-cookbooks/yum-chef/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/chef-cookbooks/apt-chef' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/apt-chef/issues' if respond_to?(:issues_url)
